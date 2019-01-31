edcb6ba50d0b765dd73a6ce0452ba45991d25da2
Junyoung McArdle <juny.mcardle@gmail.com>
2019-01-31 19:08:40 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -55,7 +55,7 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  for phrase in counter..number_of_times do
+  for for_iterator in counter..number_of_times do
     puts phrase
     counter += 1
   end
